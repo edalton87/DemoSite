e83daa475ee0d096d17d2c9c6506f3525e06ae30
Edward Dalton <evido87@yahoo.com>
2018-03-10 17:21:36 +0100

    fix: css file perfected

--- src/resources/script.js
+++ src/resources/script.js
@@ -1,30 +1,30 @@
 function revealMessage(chore, done) {
-  var message
+  var message;
   if (done) {
     switch(chore) {
     case 'dishes':
       swal({
         text: 'Thank you for doing the dishes',
         icon: "success"
-      })
+      });
         break;
     case 'room':
       swal({
         text: 'Thank you for cleaning your room',
         icon: "success"
-      })
+      });
       break;
     case 'trash':
       swal({
         text: 'Thank you for taking out the trash',
         icon: "success"
-      })
+      });
     break;
     default:
       swal({
         text: 'Good job!',
         icon: "success"
-      })
+      });
       break;
     }
   } else {
@@ -33,25 +33,25 @@ function revealMessage(chore, done) {
       swal({
         text: 'Don\'t eat like a pig!',
         icon: 'warning'
-      })
+      });
       break;
     case 'room':
       swal({
         text: 'Don\'t live like a hobo!',
         icon: 'warning'
-      })
+      });
       break;
     case 'trash':
         swal({
           text: 'Don\'t be a bum!',
           icon: 'warning'
-        })
+        });
         break;
     default:
       swal({
         text: 'Go do it!',
         icon: 'warning'
-      })
+      });
       break;
     }
   }
@@ -63,34 +63,34 @@ function strikeSuggestion(activity) {
       swal({
         text: 'You could call another one',
         icon: 'success'
-      })
+      });
       break;
     case 'rideBike':
       swal({
         text: 'That\'s good exercice',
         icon: 'success'
-      })
+      });
       break;
     case 'watchTv':
       swal({
         text: 'Watching TV is a waste of time anyway',
         icon: 'success'
-      })
+      });
       break;
     default:
       swal({
         text: 'Great!',
         icon: 'success'
-      })
+      });
       break;
   }
-  document.getElementById(activity).style.textDecorationLine = 'line-through'
-  document.getElementById(activity + 'Button').disabled = true
+  document.getElementById(activity).style.textDecorationLine = 'line-through';
+  document.getElementById(activity + 'Button').disabled = true;
 }
 
 function displaySomething() {
   swal({
     text: 'Pas mal!',
     icon: 'success'
-  })
+  });
 }
